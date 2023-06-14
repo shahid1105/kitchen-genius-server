dfadfaa0f87c30b7c73b7d82d5a75d777958b120
shahid1105 <shahidhasan1105@gmail.com>
2023-06-14 19:29:41 +0600

    chefs data get

--- index.js
+++ index.js
@@ -8,12 +8,23 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-
+const chefs = require('./data/chef.json')
 
 
 app.get('/', (req, res) => {
     res.send('chef is running')
 })
+
+
+app.get("/chefs", (req, res) => {
+    res.send(chefs)
+})
+
+
+
+
+
+
 
 app.listen(port, () => {
     console.log(`chef is running on port ${port}`);
